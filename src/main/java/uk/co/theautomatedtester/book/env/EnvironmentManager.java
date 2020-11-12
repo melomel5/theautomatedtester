e4532d6a636dac5fe3097ae9808f65374aa3ce2c
Serhii Nikolaiev <snikolaiev@readdle.com>
2020-11-12 18:05:26 +0200

    Add: Windows and Linux support

--- src/main/java/uk/co/theautomatedtester/book/env/EnvironmentManager.java
+++ src/main/java/uk/co/theautomatedtester/book/env/EnvironmentManager.java
@@ -8,7 +8,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class EnvironmentManager {
 
     public static void initWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        String osName = System.getProperty("os.name");
+        String chromeDriverPath;
+
+        if (osName.contains("Mac OS")) {
+            chromeDriverPath = "src/main/resources/drivers/mac/chromedriver";
+        } else if (osName.contains("Windows")) {
+            chromeDriverPath = "src/main/resources/drivers/win/chromedriver.exe";
+        } else if (osName.contains("Linux")) {
+            chromeDriverPath = "src/main/resources/drivers/linux/chromedriver";
+        } else {
+            throw new IllegalArgumentException("Unknown OS: " + System.getProperty("os.name"));
+        }
+
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
